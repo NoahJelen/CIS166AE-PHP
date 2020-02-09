d5991f343a46f7c753a43e187e372f7e250d519b
Noah_Jelen <noahtjelen@gmail.com>
2020-02-09 11:54:08 -0700

    next commit

---
+++ variables.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Variables</title>
+    </head>
+    <body>
+        <?php
+            //address
+            $firstName="Noah";
+            $lastName="Jelen";
+            $address="6135 North 35th Avenue";
+            $phone="602-864-0580";
+            //$city="Phoenix";
+            //$state="Arizona";
+            //$zip="85017";
+
+            //display the address
+            print "
+                <p>Information:
+                    <br>Address: $address
+                    <br>Name: $firstName $lastName
+                    <br>Phone Number: $phone
+                </p>";
+        ?>  
+    </body>
+</html>
