2bfe9277e1d3ac88591d1456027ac3fd6d275b04
Noah_Jelen <noahtjelen@gmail.com>
2020-02-09 12:06:50 -0700

    next commit

---
+++ predefined.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Variables</title>
+    </head>
+    <body>
+        <pre>
+            <?php
+                print_r($_SERVER);
+            ?>
+        </pre>
+    </body>
+</html>
