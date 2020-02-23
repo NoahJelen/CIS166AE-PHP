6fc31c835d0f80486579abc0e0236a98f91290c8
Noah_Jelen <noahtjelen@gmail.com>
2020-02-23 14:14:27 -0700

    next commit

---
+++ random.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Lucky Numbers</title>
+</head>
+<body>
+<?php
+$n1 = mt_rand(1, 100);
+$n2 = mt_rand(1, 100);
+$n3 = mt_rand(1, 100);
+
+// Print out the numbers:
+print "<p>The numbers are:<br>
+$n1<br>
+$n2<br>
+$n3</p>";
+
+?>
+</body>
+</html>
