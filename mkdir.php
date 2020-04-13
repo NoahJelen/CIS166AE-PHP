fbc15b8eb9830ebd81649c52e352babf130d91ab
Noah_Jelen <noahtjelen@gmail.com>
2020-04-12 21:53:02 -0700

    next commit

---
+++ mkdir.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Create directory</title>
+    </head>
+    <body>
+        <pre>
+            <?php
+                mkdir("/opt/lampp/htdocs/test");
+                print("Created a directory");
+            ?>
+        </pre>
+    </body>
+</html>
